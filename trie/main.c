7bd21b74f76f22ee389e0ac4eaa166c7983dbbce
gustavohassegawa <gustavohassegawa@gmail.com>
2025-07-18 18:11:33 -0300

    Implementado verificação de texto para Trie m=26

--- trie/main.c
+++ trie/main.c
@@ -1,26 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "trie.h"
+#include <ctype.h>
+#include <string.h>
+
+
+void verificar_palavras(TrieNode raiz, const char *frase) {
+    int i = 0, j = 0;
+    int len = strlen(frase);
+    char palavra[128];  
+
+    while (i <= len) {
+        if (!isspace(frase[i]) && frase[i] != '\0') {
+            if (j < 127)
+                palavra[j++] = tolower(frase[i]);  
+        } else if (j > 0) {
+            palavra[j] = '\0';
+            if (searchWord(raiz, palavra)) {
+                printf("'%s' encontrada na trie\n", palavra);
+            }
+            j = 0;
+        }
+        i++;
+    }
+}
+
+void read_string_getchar(char **str) {
+    int capacityString = 10, tamanho = 0;
+    char c; 
+    *str = malloc(capacityString * sizeof(char));
+
+    while ((c = getchar()) != '\n' && c != EOF) {
+        if (tamanho + 1 >= capacityString) {
+            capacityString *= 2;
+            *str = (char *)realloc(*str, capacityString * sizeof(char));
+        }
+
+        (*str)[tamanho++] = (char)c;
+    }
+    (*str)[tamanho] = '\0';
+}
 
 int main() {
-    /*
-    TrieNode root = createNode();
+    printf("\033[?1049h");
+    printf("\033[H\033[J");
 
-    insertWord(root, "casa");
-    insertWord(root, "carro");
-    insertWord(root, "cachorro");
+    TrieNode raiz = createNode();
+    char * palavra;
+    char * texto;
 
-    printf("%s\n", searchWord(root, "casa") ? "Encontrado" : "Não encontrado");
-    printf("%s\n", searchWord(root, "cas") ? "Encontrado" : "Não encontrado");
-    printf("%s\n", searchWord(root, "carro") ? "Encontrado" : "Não encontrado");
-
-    freeTrie(root);
-    return 0;
-    */
-
+    printf("------INSIRA AS PALAVRAS NA LISTA NEGRA------\n");
     while(1) {
-        int capacityString = 10, tamanho = 0;
-        char c, *str = malloc(capacityString);
-
-        while
+        read_string_getchar(&palavra);
+        if (palavra[0] == '\0') {
+            free(palavra);
+            break;
+        }
+        insertWord(raiz, palavra);
+        free(palavra);
     }
+
+    printf("\033[H\033[J");
+    printf("------INSIRA O TEXTO A SER VERIFICADO------\n");
+    
+    while(1) {
+        read_string_getchar(&texto);
+        if (texto[0] == '\0') {
+            free(texto);    
+            break;
+        }
+        printf("\n");
+        verificar_palavras(raiz, texto);
+        free(texto);
+    }
+    freeTrie(raiz);
+    printf("\033[?1049l");
+    return 0;
 }
